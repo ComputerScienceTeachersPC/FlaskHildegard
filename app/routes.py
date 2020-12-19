f5985478e74b0713b4d0da1460d79cdd09170d90
Grayson Solutions <jenny.andrews@grayson-solutions.com>
2020-12-19 16:00:49 +0000

    Add end def comments

--- app/routes.py
+++ app/routes.py
@@ -28,12 +28,15 @@ def homepage():
         print("ERROR - Getting user from db")
         return render_template("index.html", class_data = [])
     #end try
-    
+#end def    
 
 
 @app.errorhandler(405)
 def wrong_method_405(error):
     return render_template("error.html", error="You are a numpty. You appear to have tried to access a page that doesnt exist")
+#end  def
+
 @app.errorhandler(404)
 def wrong_method_404(error):
     return render_template("error.html", error="You appear to have tried to access a page that doesnt exist. Try and use the navigation menu, rather than entering url's manually")
+#end def
